d8e9579af82893790ebfdb8040cfdddc6813fcbb
Jason - CrankyCoder <jason.brunk@gmail.com>
2016-04-25 13:02:28 -0400

    Initial Checkin
    desensitized the code

---
+++ index.php
@@ -0,0 +1,38 @@
+<?PHP
+
+	// This will prevent any errors that occur from potentially displaying your username/password
+	error_reporting(0);
+
+	// This should properly display device names that contain special characters
+	header("Content-type: text/html; charset=utf-8");
+
+	// Include the FindMyiPhone class
+	include ("class.findmyiphone.php");
+	
+	// This is where we log in to iCloud
+	try {
+	} catch (Exception $e) {
+		print "Error: ".$e->getMessage();
+		exit;
+	}
+	
+	// This will print out all the devices on your account so you can grab the device IDs
+	//$fmi->printDevices();
+        //$fmi->playSound("cQJeTYA+0cL/CY2IF0ZgZdGBLFlc+C6FQnYZNc26q8R4k9S/5lVh5eHYVNSUzmWV","test");
+	// Find a device that is reporting its location and attempt to get its current location
+
+
+
+
+        switch ($_GET["phone"])
+		{
+		case "jason":
+		$fmi->playSound($jasonphone,"Find Me!!!");
+		break;
+		
+		case "ipad";
+		$fmi->playSound($ipadmini,"Find Me!!");
+                break;
+                }
+ 
+?>
